538ae2066ba653539bffdb76416f8679f491bb00
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:34:49 +0530

    Create ten_lines.c

---
+++ c_programming/loops/while_loop/ten_lines.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+printf("\nUsing a while loop:\n");
+    int i = 1;
+    while (i <= 2) {
+        printf("Line %d\n", i);
+        ++i;
+    }
+    return 0;
+}
