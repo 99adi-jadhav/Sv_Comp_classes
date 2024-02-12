c0e017bf2d2e7faa71e8488cd0ffc0029b2575b1
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:26:39 +0530

    Update if_else7.c

--- c_programming/if_else_statements/if_else7.c
+++ c_programming/if_else_statements/if_else7.c
@@ -2,11 +2,16 @@
 int main()
 {
   int hour = 15;
-    if (hour < 12) {
+    if (hour < 12) 
+    {
         printf("Good morning!\n");
-    } else if (hour < 18) {
+    } 
+    else if (hour < 18) 
+    {
         printf("Good afternoon!\n");
-    } else {
+    } 
+    else 
+    {
         printf("Good evening!\n");
     }
       return 0;
