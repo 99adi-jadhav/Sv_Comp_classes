b3eefa8762049460a7a371ee7310a47c6b1b7b11
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:25:03 +0530

    Create if_else5.c

---
+++ c_programming/if_else_statements/if_else5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{    
+    int value = -8;
+    if (value < 0) 
+    {
+        printf("The value is negative.\n");
+    } else if (value > 0)
+     {
+        printf("The value is positive.\n");
+     } 
+    else 
+    {
+        printf("The value is zero.\n");
+    }
+       return 0;
+ }
+
+    
