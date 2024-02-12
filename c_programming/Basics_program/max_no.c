3b081c59fdc0e7dc0867458986921ce640cc91ed
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:09:21 +0530

    Create max_no.c

---
+++ c_programming/Basics_program/max_no.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("Enter your first num:\n");
+    scanf("%d",&num1);
+
+printf("Enter your second number num:\n");
+    scanf("%d",&num2);
+
+    if(num1>num2){
+
+       printf("first number is maximum\n");
+
+    }
+    else if(num1<num2){
+       
+       printf("your second number is maximum\n");
+    
+    }
+
+    else{
+
+        printf("both are equal\n"); 
+
+    }
+        return 0;
+
+}
