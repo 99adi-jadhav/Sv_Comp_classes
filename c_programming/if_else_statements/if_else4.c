93c23a831e62cff54ca12764990453667fd9803b
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:24:35 +0530

    Create if_else4.c

---
+++ c_programming/if_else_statements/if_else4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{    char grade = 'B';
+    if (grade == 'A') {
+        printf("Excellent!\n");
+    } else if (grade == 'B') {
+        printf("Good job!\n");
+    } else {
+        printf("Keep working hard!\n");
+    }
+        return 0;
+}
