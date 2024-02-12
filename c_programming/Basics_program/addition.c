ea0f3faa8a24fa19dc198a604b260e4920c9a7e6
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:05:57 +0530

    Create addition.c

---
+++ c_programming/Basics_program/addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,add;
+    printf("Enter The First Number : ");
+    scanf("%d",&num1);
+    printf("Enter The Second Number : ");
+    scanf("%d",&num2);
+    add = num1 + num2;
+    printf("Addition Of Two Number is : %d",add);
+    return 0;
+}
