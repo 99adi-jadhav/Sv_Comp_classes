8486362045fd6a5d0d66c678de3c2c891f2c2ba5
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:46:10 +0530

    Create scanf1.c

---
+++ c_programming/Basics_program/use_of_scanf_function/scanf1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sub;
+    printf("Enter The First Number : ");
+    scanf("%d",&num1);
+    printf("Enter The Second Number : ");
+    scanf("%d",&num2);
+    sub = num1 - num2;
+    printf("Substraction Of Two Number is : %d",sub);
+    return 0;
+}
