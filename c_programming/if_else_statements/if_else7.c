2e1a5db1db7d2042b6df8b08585273ea5cea9b7f
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:25:55 +0530

    Create if_else7.c

---
+++ c_programming/if_else_statements/if_else7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int hour = 15;
+    if (hour < 12) {
+        printf("Good morning!\n");
+    } else if (hour < 18) {
+        printf("Good afternoon!\n");
+    } else {
+        printf("Good evening!\n");
+    }
+      return 0;
+}
+
+   
