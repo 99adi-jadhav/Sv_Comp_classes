1b22a03255c5479eb1f0809ee54d23563f324da8
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:11:32 +0530

    Create odd.c

---
+++ c_programming/Basics_program/odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,mul;
+    printf("Enter The First Number : ");
+    scanf("%d",&num1);
+    if (num1 % 2 == 1)
+    {
+        printf("Given Number Is Odd Number ");
+    }
+    else
+    {
+        printf("Given Number Is Not Odd NUmber");
+    }
+    return 0;
+}
