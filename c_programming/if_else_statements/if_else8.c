28b59a37bb5bb1f86f554ecfc0a527f9e5cbf0ec
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:27:30 +0530

    Create if_else8.c

---
+++ c_programming/if_else_statements/if_else8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int temperature = 25;
+    if (temperature > 30)
+    {
+        printf("It's hot outside!\n");
+    } 
+    else if (temperature >= 20) 
+    {
+        printf("The weather is pleasant.\n");
+    } 
+    else 
+    {
+        printf("It's cold outside!\n");
+    }
+    return 0;
+}
