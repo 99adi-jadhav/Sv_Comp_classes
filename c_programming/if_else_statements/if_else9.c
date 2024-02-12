beebdca2c82b0c3e2aab6b6e483d7c36eb2a9c33
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:28:11 +0530

    Create if_else9.c

---
+++ c_programming/if_else_statements/if_else9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num1 = 8, num2 = 12;
+    if (num1 == num2)
+    {
+        printf("Both numbers are equal.\n");
+    } 
+    else 
+    {
+        printf("The numbers are not equal.\n");
+    }
+  return 0;
+}
