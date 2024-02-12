d339d8b65c39bb58ff00c3387ae8668d079c4f0e
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:16:32 +0530

    Create div.c

---
+++ c_programming/Basics_program/div.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,div;
+    printf("Enter The First Number : ");
+    scanf("%d",&num1);
+    printf("Enter The Second Number : ");
+    scanf("%d",&num2);
+    div = num1 / num2;
+    printf("Division Of Two Number is : %d",div);
+    return 0;
+}
