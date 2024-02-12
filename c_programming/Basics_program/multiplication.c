a0271e072669dcab278696a19de5db0150fac50f
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:10:48 +0530

    Create multiplication.c

---
+++ c_programming/Basics_program/multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,mul;
+    printf("Enter The First Number : ");
+    scanf("%d",&num1);
+    printf("Enter The Second Number : ");
+    scanf("%d",&num2);
+    mul = num1 * num2;
+    printf("Multiplication Of Two Number is : %d",mul);
+    return 0;
+}
