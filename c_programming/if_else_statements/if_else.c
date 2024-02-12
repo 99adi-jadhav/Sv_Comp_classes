1b80bc593c3664ab5f4178446d7f4958c703852c
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:19:47 +0530

    Update if_else.c

--- c_programming/if_else_statements/if_else.c
+++ c_programming/if_else_statements/if_else.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+
+    int num = 5;
+    if (num > 0) 
+    {
+        printf("The number is positive.\n");
+    }
+     else
+     {
+        printf("The number is non-positive.\n");
+        return 0;
+     }
+}
+    
