9c8db69bb55fdd3a056c20defc1c4b54df68641a
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 00:32:13 +0530

    Create for_loop_odd_1to10.c

---
+++ c_programming/loops/for_loop_odd_1to10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("Print odd numbers between 1 and 10 using a for loop:\n");
+
+    for (int i = 1; i <= 10; i += 2) 
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
