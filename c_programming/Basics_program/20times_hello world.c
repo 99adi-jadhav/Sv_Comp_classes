43a71a138a9fbddfed5390afa8dd34b595356cd2
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 19:21:51 +0530

    Update Basics_program

---
+++ c_programming/Basics_program/20times_hello world.c
@@ -0,0 +1,27 @@
+#include<studio.h>
+int main ()
+{
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World"); 
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+    printf(Hello World");
+     return 0;
+}
+
+Output = Hello World
