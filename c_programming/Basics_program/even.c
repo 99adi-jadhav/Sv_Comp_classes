0a5b55974467132a9323c59cb032c80fa513b5f8
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 20:43:44 +0530

    Update even.c

--- c_programming/Basics_program/even.c
+++ c_programming/Basics_program/even.c
@@ -2,7 +2,7 @@
 int main()
 {
     int num1,num2,mul;
-    printf("Enter The First Number : ");
+    printf("Enter The Number : ");
     scanf("%d",&num1);
     if (num1 % 2 == 0)
     {
@@ -14,3 +14,8 @@ int main()
     }
     return 0;
 }
+
+
+Output = Enter The Number : 
+          11
+          Given Number Is Not Even NUmber
