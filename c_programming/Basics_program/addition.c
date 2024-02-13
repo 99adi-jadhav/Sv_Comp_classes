c4d84bf2c0f7183b5292775d79975adcd4d6ed58
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 10:19:57 +0530

    Update addition.c

--- c_programming/Basics_program/addition.c
+++ c_programming/Basics_program/addition.c
@@ -2,11 +2,16 @@
 int main() //Entry Point Function
 {
     int num1,num2,add;
-    printf("Enter The First Number : ");
+    printf("Enter The First Number : "\n);
     scanf("%d",&num1);
-    printf("Enter The Second Number : ");
+    printf("Enter The Second Number : "\n);
     scanf("%d",&num2);
     add = num1 + num2;
-    printf("Addition Of Two Number is : %d",add);
+    printf("\nAddition Of Two Number is : %d",add);
     return 0;
 }
+
+
+Output = Enter The First Number : 10
+         Enter The Second Number : 20
+         Addition Of Two Number is : 30
