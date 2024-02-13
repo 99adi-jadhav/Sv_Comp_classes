f8fc72c7e05ba57eeb7d1b1389a971ab3b0f84bc
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-13 19:30:05 +0530

    Update 20times_hello world.c

--- c_programming/Basics_program/20times_hello world.c
+++ c_programming/Basics_program/20times_hello world.c
@@ -1,27 +1,46 @@
 #include<studio.h>
 int main ()
 {
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World"); 
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
-    printf(Hello World");
+    printf(Hello World/n");
+    printf(Hello World\n");
+    printf(Hello World\n"); 
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
+    printf(Hello World\n");
      return 0;
 }
 
 Output = Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World
+         Hello World 
+         Hello World
+         Hello World
+         Hello World
