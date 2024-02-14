8d41431b36712320357d07414bc5719272aeb8f1
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-14 16:36:55 +0530

    Update 1to10_lines.c

--- c_programming/loops/for_loop/1to10_lines.c
+++ c_programming/loops/for_loop/1to10_lines.c
@@ -11,3 +11,14 @@ int main()
 
     return 0;
 }
+
+Output = Print numbers 1 to 10 using a for loop: 1
+          2
+          3
+          4
+          5
+          6
+          7
+          8
+          9
+          10
