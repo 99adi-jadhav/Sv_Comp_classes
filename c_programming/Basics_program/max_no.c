3284eef6c200d1e98e5981858c926578c99ee700
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-14 16:20:48 +0530

    Update max_no.c

--- c_programming/Basics_program/max_no.c
+++ c_programming/Basics_program/max_no.c
@@ -1,29 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    int num1,num2;
-    printf("Enter your first num:\n");
-    scanf("%d",&num1);
+int num1,num2;
+ printf("\nEnter your first number : ");
+ scanf("%d",&num1);
 
-printf("Enter your second number num:\n");
-    scanf("%d",&num2);
+ printf("\nEnter your second number : ");
+ scanf("%d",&num2);
 
-    if(num1>num2){
-
-       printf("first number is maximum\n");
-
+  if(num1>num2)
+    {
+     printf("first number is maximum\n");
     }
-    else if(num1<num2){
-       
-       printf("your second number is maximum\n");
-    
+    else if(num1<num2)
+    {
+     printf("Second Number Is Maximum\n");
     }
-
-    else{
-
-        printf("both are equal\n"); 
-
+  else
+    {
+     printf("both are equal\n"); 
     }
-        return 0;
-
+       return 0;
 }
+
+
+Output = Enter your first number : 40
+         Enter your second number : 60
+         Second Number Is Maximum   
