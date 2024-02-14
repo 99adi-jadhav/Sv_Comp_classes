286dc5dd932a373ce4eb683911f241140ac0f712
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-14 23:55:46 +0530

    Create daigonal.c

---
+++ c_programming/patteren matching/without_loop/daigonal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    printf("\t *  \n");
+    printf("        \t *  \n");
+    printf("                \t *  \n");
+    printf("                        \t *  \n");
+    return 0;
+    
+}
+
+
+Output :
+               *  
+                   *  
+                         *  
+                               *  
