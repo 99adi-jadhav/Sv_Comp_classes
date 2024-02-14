c98c15729e30747f3a246fecfd00aa481c0d0fe5
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-14 23:47:05 +0530

    Create table.c

---
+++ c_programming/patteren matching/with_loop/table.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+    int main()
+        {
+           int i;
+              for(i = 2;i <= 20; i = i + 2)
+                {
+                    printf("%d \n",i);
+               }
+                     return(0);
+        }
+
+  Output :     
+    2
+    4
+    6
+    8
+    10
+    12
+    14
+    16
+    18
+    20
