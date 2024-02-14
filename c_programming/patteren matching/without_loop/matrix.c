81a222ea062d17719687c3f2a16c0fe2b6d0c5f2
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-14 23:54:09 +0530

    Create matrix.c

---
+++ c_programming/patteren matching/without_loop/matrix.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    printf("\t * \t * \t * \t * \n");
+    printf("\t * \t * \t * \t * \n");
+    printf("\t * \t * \t * \t * \n");
+    printf("\t * \t * \t * \t * \n");
+    printf("\t * \t * \t * \t * \n");
+    printf("\t * \t * \t * \t * \n");
+    return 0;
+    
+}
+
+Output :  
+
+         *       *       *       * 
+         *       *       *       * 
+         *       *       *       * 
+         *       *       *       * 
+         *       *       *       * 
+         *       *       *       * 
