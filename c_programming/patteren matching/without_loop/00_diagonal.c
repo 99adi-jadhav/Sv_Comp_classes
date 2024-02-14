25972d990d318483a1feb23f6ccab2fa4762223a
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-15 00:00:52 +0530

    Create 00_diagonal.c

---
+++ c_programming/patteren matching/without_loop/00_diagonal.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    printf("\t 00  \n");
+    printf("        \t 11  \n");
+    printf("                \t 22  \n");
+    printf("                        \t 33  \n");
+    return 0;
+    
+}
+
+
+Output :  00  
+                 11  
+                         22  
+                                 33  
