6718cbbf4a2012a2c791366e119b56e59230ad56
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-15 00:48:27 +0530

    Update use_of_scanf_function

---
+++ c_programming/Basics_program/use_of_scanf_function/scanf6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,mul;
+    printf("Enter The First Number : ");
+    scanf("%d",&num1);
+    printf("Enter The Second Number : ");
+    scanf("%d",&num2);
+    mul = num1 * num2;
+    printf("Multiplication Of Two Number is : %d",mul);
+    return 0;
+}
+
+Output : Enter The First Number : 5
+         Enter The Second Number : 2
+         Multiplication Of Two Number is : 10
