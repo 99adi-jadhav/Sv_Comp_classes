ddb294269047abdaac273f4daf6ce2c3c4159a91
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-14 23:49:55 +0530

    Create matrix.c

---
+++ c_programming/patteren matching/with_loop/matrix.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+    int main()
+        {
+           int i;
+              for(i = 0;i < 4; i++)
+                    {
+                        {
+                             for(int j = 0;j < 4; j++)
+                             printf(" * \t ");
+                        }
+
+                            printf("\n");
+                    }
+                            
+                        return(0);
+        }
+
+Output :
+ *        *       *       *      
+ *        *       *       *      
+ *        *       *       *      
+ *        *       *       *  
