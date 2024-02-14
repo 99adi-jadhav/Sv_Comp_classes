e92db3bfc51e4d330d40972a47e74e367f1804c7
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-14 23:57:13 +0530

    Create add_1t010.c

---
+++ c_programming/patteren matching/without_loop/add_1t010.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num1=1,num2=2,num3=3,num4=4,num5=5,num6=6,num7=7,num8=8,num9=9,num10=10,add=0;
+    
+        add = num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+            
+            printf("\nAddition Of Two Number is : %d\n",add);
+            
+             return 0;
+}
+
+
+
+Output :         Addition Of Two Number is : 55
+ 
