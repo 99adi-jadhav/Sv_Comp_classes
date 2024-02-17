831c5e498022287bdc146067110c27ec7d38613c
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-17 12:43:22 +0530

    Update if_else1.c

--- c_programming/if_else_statements/if_else1.c
+++ c_programming/if_else_statements/if_else1.c
@@ -5,11 +5,14 @@ int main()
     
     if (age >= 18)
      {
-        printf("You are eligible to vote.\n");
+        printf("You are eligible to vote \n");
      } 
      else 
      {
-        printf("You are not eligible to vote.\n");
+        printf("You are not eligible to vote \n");
      }
      return 0;
 }
+
+
+Output : You are eligible to vote
