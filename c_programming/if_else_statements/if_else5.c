18b3f0c10b8547a751bf1cca4ddd102fcb8016b6
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-17 12:37:40 +0530

    Update if_else5.c

--- c_programming/if_else_statements/if_else5.c
+++ c_programming/if_else_statements/if_else5.c
@@ -4,16 +4,17 @@ int main()
     int value = -8;
     if (value < 0) 
     {
-        printf("The value is negative.\n");
+        printf("The value is negative \n");
     } else if (value > 0)
      {
-        printf("The value is positive.\n");
+        printf("The value is positive \n");
      } 
     else 
     {
-        printf("The value is zero.\n");
+        printf("The value is zero \n");
     }
        return 0;
  }
 
     
+Output : The value is negative
