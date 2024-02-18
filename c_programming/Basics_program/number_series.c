55cb4946ac9444a3fa519cd370c68fded5531c72
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-18 16:12:28 +0530

    Create number_series.c

---
+++ c_programming/Basics_program/number_series.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    for(int i = -5;i <= 5; i++ )
+    {
+        printf("%d \t",i);
+    }
+    return 0;
+}
+
+
+Output = -5      -4      -3      -2      -1      0       1       2       3       4       5 
