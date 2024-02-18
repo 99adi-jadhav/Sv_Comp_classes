51c1536affac200cd6f4a3b513637de5c2338834
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-18 16:27:06 +0530

    Create loops_syantax.c

---
+++ c_programming/loops/for_loop/loops_syantax.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+int main()
+{
+    printf("Print First loop \n");
+    int i;
+    for(i = 0;i < 10; i++)
+    {
+        printf("%d \n",i);
+    }
+    return 0;
+
+}
+
+
+Output : 
+Print First loop 
+0 
+1 
+2 
+3 
+4 
+5 
+6 
+7 
+8 
+9 
+
+
+
+// #include<stdio.h>
+
+// int main()
+// {
+
+
+// 	printf("First for loop\n");
+// 	int i;
+// 	for(i = 0; i < 10; i++)
+// 	{
+// 		printf(" %d ",i);
+// 	}
+
+// 	printf("Second for loop\n");
+// 	i = 0;
+// 	for( ; i < 10; i++)
+// 	{
+// 		printf(" %d ",i);
+// 	}
+
+// 	printf("Third for loop\n");
+// 	i = 0;
+// 	for( ; i < 10; )
+// 	{
+// 		i++;
+// 		printf(" %d ",i);
+// 	}
+
+// 	printf("Fourth for loop\n");
+// 	i = 0;
+// 	for( ; ; )
+// 	{
+// 		i++;
+// 		if(i<10)
+// 		{
+// 			printf(" %d ",i);
+// 		}
+// 		else
+// 		{
+// 			break;
+// 		}
+// 	}
+// }
+
+	
+	
