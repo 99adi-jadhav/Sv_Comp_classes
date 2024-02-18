919b9afeb7a722a80900dee21d6082b51956e6e2
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-18 16:10:43 +0530

    Create even_odd.c

---
+++ c_programming/Basics_program/even_odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a = 8;
+    if (a % 2 == 0)
+    {
+        printf("Given Number Is Even Number ");
+    }
+    else
+    {
+        printf("Given Number Is Odd NUmber");
+    }
+    return 0;
+}
+
+
+Output = Given Number Is Even Number
