459d8a4573c43ce509d900c96b2e1fa6afde686b
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-18 16:31:42 +0530

    Create do_while_syantax.c

---
+++ c_programming/loops/do_while/do_while_syantax.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+    printf("First do while loop\n");
+	int i = 0;
+	do
+	{
+		printf(" %d ",i);
+		i++;
+	}
+    while(i!=10);
+	return 0;
+}
+
+
+Output = First do while loop
+ 0  1  2  3  4  5  6  7  8  9 
+
+// printf("First do while loop\n");
+// i = 0;
+// 	do
+// 	{
+// 		printf(" %d ",i);
+// 		i++;
+// 	}   while(i!=10);
+
+
+
+
+// 	printf("Second do while loop\n");
+// 	i = 10;
+// 	do
+// 	{
+// 		printf(" %d ",i);
+// 	}while(i!=10);
+
+
+
+
+// 	while(i!=10)
+// 	{
+// 		printf(" %d ",i);
+// 	}
+// 	    return 0;
+// }
+
+
