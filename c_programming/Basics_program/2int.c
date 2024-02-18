473f68b46a150716629e715fa72e05622a611f2f
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-18 16:06:51 +0530

    Create 2int.c

---
+++ c_programming/Basics_program/2int.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a =10;
+    int b =20;
+    printf("The Value of a is : %d\n",a);
+    printf("The Value of b is : %d\n",b);
+
+    return 0;
+}
+
+Output :
+          The Value of a is : 10
+          The Value of b is : 20
