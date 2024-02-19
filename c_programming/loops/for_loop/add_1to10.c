a613edd059f1add91e548e01f5f8499b1a23761d
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-19 16:11:44 +0530

    Create add_1to10.c

---
+++ c_programming/loops/for_loop/add_1to10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int add = 0;
+    for(int i = 1;i <= 10 ;i++)
+    {
+         add += i;
+    }
+        printf(" The Addition is : %d \n",add);
+    return 0;
+}
+
+Output = 
+
+ The Addition is : 55
