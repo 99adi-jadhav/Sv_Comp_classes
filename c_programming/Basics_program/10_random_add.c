2ade14afa43406de18e334aeaca1f94438be659b
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-19 16:06:54 +0530

    Create 10_random_add.c

---
+++ c_programming/Basics_program/10_random_add.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3,num4,num5,num6,num7,num8,num9,num10,add;
+    printf("\nEnter The First Number : ");
+    scanf("%d",&num1);
+    printf("\nEnter The Second Number : ");
+    scanf("%d",&num2);
+    printf("\nEnter The Third Number : ");
+    scanf("%d",&num3);
+    printf("\nEnter The Forth Number : ");
+    scanf("%d",&num4);
+    printf("\nEnter The Fifth Number : ");
+    scanf("%d",&num5);
+    printf("\nEnter The Sixth Number : ");
+    scanf("%d",&num6);
+    printf("\nEnter The Seventh Number : ");
+    scanf("%d",&num7);
+    printf("\nEnter The Eighth Number : ");
+    scanf("%d",&num8);
+    printf("\nEnter The Nineth Number : ");
+    scanf("%d",&num9);
+    printf("\nEnter The Tenth Number : ");
+    scanf("%d",&num10);
+    add = num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+    printf("\nAddition Of Two Number is : %d \n",add);
+    return 0;
+}
+
+Output = 
+  
+Enter The First Number : 1
+
+Enter The Second Number : 1
+
+Enter The Third Number : 1
+
+Enter The Forth Number : 1
+
+Enter The Fifth Number : 1
+
+Enter The Sixth Number : 1
+
+Enter The Seventh Number : 1
+
+Enter The Eighth Number : 1
+
+Enter The Nineth Number : 1
+
+Enter The Tenth Number : 1
+
+Addition Of Two Number is : 10 
