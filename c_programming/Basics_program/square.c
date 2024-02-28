86818aa3438a896d557623097efe2e3696d031f8
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-28 18:34:31 +0530

    Create square.c

---
+++ c_programming/Basics_program/square.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a,square = 0;
+    printf("\n Enter the Number : ");
+    scanf("%d",&a);
+
+    square = a*a;
+
+    printf("The Square of Number is : %d \n",square);
+    return 0;
+}
+
+
+Output : 
+
+Enter the Number : 10
+The Square of Number is : 100 
+
+
+
+
+
