807073a13cda8ab6f0cd3049310db4769dacffed
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 00:39:54 +0530

    Update and rename square.c to 25_find_square_of_number.c

---
+++ c_programming/Basics_program/25_find_square_of_number.c
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////
+// Date : 29 Feb 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program to find square of the given number.....
+// Concept : Basics Program
+// Expected Output :     Enter the Number : 10
+//                       The Square of Number is : 100 
+//////////////////////////////////////////////////////////////
+
+
+
+
+
+#include<stdio.h>
+int main()
+{
+    int a,square = 0;
+    printf("\n Enter the Number : ");
+    scanf("%d",&a);
+
+    square = a*a;
+
+    printf("The Square of Number is : %d \n",square);
+    return 0;
+}
+
+
+Output : 
+
+Enter the Number : 10
+The Square of Number is : 100 
+
+
+
+
+
