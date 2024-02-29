06ce332a05e2faf6acbc1a84336114cd686f39a1
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 01:05:32 +0530

    Update 05_use_of_scanf_function.c

--- c_programming/Basics_program/use_of_scanf_function/05_use_of_scanf_function.c
+++ c_programming/Basics_program/use_of_scanf_function/05_use_of_scanf_function.c
@@ -1,3 +1,14 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program to print seprated integer ......
+// Concept : Use of Scanf Function
+// Expected Output :   Enter two integers separated by a space: 12 30
+//                     You entered: 12 and 30
+//////////////////////////////////////////////////////////////
+
+
+
 #include<stdio.h>
 int main() {
  int a, b;
