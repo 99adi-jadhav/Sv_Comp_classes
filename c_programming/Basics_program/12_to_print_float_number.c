d17b5b74a21b928a2f4cf8edad1c3f4dc6d8b86d
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 00:09:30 +0530

    Update and rename float.c to 12_to_print_float_number.c

---
+++ c_programming/Basics_program/12_to_print_float_number.c
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////
+// Date : 29 Feb 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program to print float Number.....
+// Concept : Basics Program
+// Expected Output : 10.20
+//////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+int main()
+{
+    float A = 10.20;
+    printf("The Float Value 0f A is : %f",A);
+    return 0;
+}
+
+
+Output = The Float Value 0f A is : 10.20
