66ce58bd20acbf187d1bbc6a48b8055157cbc515
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 00:33:35 +0530

    Update and rename print_two_integer.c to 23_to_print_two_integer.c

---
+++ c_programming/Basics_program/23_to_print_two_integer.c
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////
+// Date : 29 Feb 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program to print two integer....
+// Concept : Basics Program
+// Expected Output :        The Value of a is : 10
+//                          The Value of b is : 20
+//////////////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+int main()
+{
+    int a =10;
+    int b =20;
+    printf("The Value of a is : %d\n",a);
+    printf("The Value of b is : %d\n",b);
+
+    return 0;
+}
+
+Output :
+          The Value of a is : 10
+          The Value of b is : 20
