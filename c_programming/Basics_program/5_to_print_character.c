7c66c275fa0af27675c94b4df7ea607cd61f381a
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-29 23:49:17 +0530

    Update and rename char.c to 5_to_print_character.c

---
+++ c_programming/Basics_program/5_to_print_character.c
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////
+// Date : 29 Feb 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program to Print a Character...            
+// Concept : Basics Program
+// Expected Output : A
+//////////////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+    int main()
+{
+        char ch = 'A';
+        printf("Character is = %c ",ch);
+        return (0); 
+}
+ 
+
+Output = Character is = A
