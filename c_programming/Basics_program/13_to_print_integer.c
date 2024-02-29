a20d2c4adb8384e1a6926a4da4ecc1498e9d0796
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 00:12:39 +0530

    Update and rename int.c to 13_to_print_integer.c

---
+++ c_programming/Basics_program/13_to_print_integer.c
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////
+// Date : 29 Feb 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program to print integer
+// Concept : Basics Program
+// Expected Output :10
+//////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+int main()
+{
+    int a =10;
+    printf("The Value of a is : %d",a);
+    return 0;
+}
+
+
+Output = The Value of a is : 10
