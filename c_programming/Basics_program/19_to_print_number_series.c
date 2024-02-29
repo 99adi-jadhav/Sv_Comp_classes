9a899abb016e02efffeb615dc0d79424790fbd68
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 00:24:47 +0530

    Update and rename number_series.c to 19_to_print_number_series.c

---
+++ c_programming/Basics_program/19_to_print_number_series.c
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////
+// Date : 29 Feb 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program to print numberseries
+// Concept : Basics Program
+// Expected Output :     -5      -4      -3      -2      -1      0       1       2       3       4       5 
+//////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+int main ()
+{
+    for(int i = -5;i <= 5; i++ )
+    {
+        printf("%d \t",i);
+    }
+    return 0;
+}
+
+
+Output = -5      -4      -3      -2      -1      0       1       2       3       4       5 
