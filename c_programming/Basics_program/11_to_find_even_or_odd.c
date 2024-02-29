bdce07311da5949f800784343a1102de5bf96bec
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 00:03:19 +0530

    Update and rename even_odd.c to 11_to_find_even_or_odd.c

---
+++ c_programming/Basics_program/11_to_find_even_or_odd.c
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////////////
+// Date : 29 Feb 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program How to find even or odd....
+// Concept : Basics Program
+// Expected Output : Given Number Is Even Number
+//////////////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+int main()
+{
+    int a = 8;
+    if (a % 2 == 0)
+    {
+        printf("Given Number Is Even Number ");
+    }
+    else
+    {
+        printf("Given Number Is Odd NUmber");
+    }
+    return 0;
+}
+
+
+Output = Given Number Is Even Number
