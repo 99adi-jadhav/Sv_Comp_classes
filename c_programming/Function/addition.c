d5ea7fe8a734d4fba2978f1d03b829cb6379fd84
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-29 12:43:55 +0530

    Create addition.c

---
+++ c_programming/Function/addition.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+        int main()
+    {
+            int num1;  
+            int num2;
+            int ans=0;
+        
+        printf("Enter value of num1 = ");
+            scanf("%d",&num1);
+
+         printf("Enter value of num2 = ");
+            scanf("%d",&num2);
+            
+             int add(int num1 ,int num2 );
+
+
+        ans = add(num1 , num2);     
+
+            printf("\n Addition  of %d & %d is = %d ",num1,num2,ans);
+
+                     return (0);
+} 
+     
+     int add(int num1 ,int num2 )
+{
+        int Addition;
+
+      Addition = num1 - num2;
+
+
+         return(Addition);
+}
+
+
+Output : 
+          Enter value of num1 = 100
+          Enter value of num2 = 200
+
+         Addition  of 100 & 200 is = 300 
