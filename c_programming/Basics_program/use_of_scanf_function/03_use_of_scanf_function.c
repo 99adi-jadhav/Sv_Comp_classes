2c9bbc036ffbb2eab4fb60c94aaa51a0ee46528f
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 01:01:15 +0530

    Update 03_use_of_scanf_function.c

--- c_programming/Basics_program/use_of_scanf_function/03_use_of_scanf_function.c
+++ c_programming/Basics_program/use_of_scanf_function/03_use_of_scanf_function.c
@@ -1,3 +1,13 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program to print a Float Number.....          
+// Concept : Use of Scanf Function
+// Expected Output :  Enter an integer: 9.5
+//                    You entered: 9.5
+//////////////////////////////////////////////////////////////
+
+
 #include<stdio.h>
 int main() {
 float floatNumber;
