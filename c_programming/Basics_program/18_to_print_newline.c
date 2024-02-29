c31eb8d5f2ddc471be732c1450c863815aaf2034
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 00:23:30 +0530

    Update and rename newline.c to 18_to_print_newline.c

---
+++ c_programming/Basics_program/18_to_print_newline.c
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////
+// Date : 29 Feb 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program to print new line....
+// Concept : Basics Program
+// Expected Output :     Hello
+//                       World
+//////////////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello\n\n");
+    printf("World\n\n");
+    return 0;
+}
+
+Output = Hello
+         World
