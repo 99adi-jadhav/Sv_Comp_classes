c44011ec33da534dda961f1c3444a4fe62778122
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-29 23:56:29 +0530

    Update and rename double.c to 8_to_print_double_values.c

---
+++ c_programming/Basics_program/8_to_print_double_values.c
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////
+// Date : 29 Feb 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program to Print to print double value....  
+// Concept : Basics Program
+// Expected Output : 3.14
+//////////////////////////////////////////////////////////////
+
+
+
+#include <stdio.h>
+
+int main() {
+    double d = 3.14;
+    printf("Num is : %lf", d);
+    return 0;
+}
+
+Output = Num is : 3.14
--- c_programming/Basics_program/double.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    double d = 3.14;
-    printf("Num is : %lf", d);
-    return 0;
-}
-
-Output = Num is : 3.14
