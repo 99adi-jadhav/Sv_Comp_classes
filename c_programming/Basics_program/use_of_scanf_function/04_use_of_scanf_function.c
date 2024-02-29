04947fa008a5619f933914b0b3112c728248246a
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 01:03:44 +0530

    Update 04_use_of_scanf_function.c

--- c_programming/Basics_program/use_of_scanf_function/04_use_of_scanf_function.c
+++ c_programming/Basics_program/use_of_scanf_function/04_use_of_scanf_function.c
@@ -1,3 +1,14 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : write a program to print a Character .... 
+// Concept : Use of Scanf Function
+// Expected Output :  Enter a character: A    
+//                    You entered: A
+//////////////////////////////////////////////////////////////
+
+
+
 #include<stdio.h>
 int main() {
 char character;
