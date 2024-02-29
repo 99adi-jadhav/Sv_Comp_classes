e851a5144d27c65977275304792ec4b163d95b73
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-02-29 13:33:37 +0530

    Create division.c

---
+++ c_programming/Function/division.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+        int main()
+    {
+            int num1;  
+            int num2;
+            int ans=0;
+        
+        printf("Enter value of num1 = ");
+            scanf("%d",&num1);
+
+         printf("Enter value of num2 = ");
+            scanf("%d",&num2);
+            
+             int div(int num1 ,int num2 );
+
+
+        ans = div(num1 , num2);     
+
+            printf("\n Division  of %d & %d is = %d ",num1,num2,ans);
+
+                     return (0);
+} 
+     
+     int div(int num1 ,int num2 )
+{
+        int Division;
+
+      Division = num1 / num2;
+
+
+         return(Division);
+}
+
+
+Output :  Enter value of num1 = 100
+          Enter value of num2 = 2
+
+          Division  of 100 & 2 is = 50
