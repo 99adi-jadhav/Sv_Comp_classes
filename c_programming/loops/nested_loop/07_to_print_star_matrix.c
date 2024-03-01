2ead52e7d1657c791c0e75a6a2cb481f8babcc2a
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 16:53:24 +0530

    Update and rename print_star.c to 07_to_print_star_matrix.c

---
+++ c_programming/loops/nested_loop/07_to_print_star_matrix.c
@@ -0,0 +1,34 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : to print lower triangle in * format...!
+// Concept : loops
+// Expected Output :  *      *      *      *
+//                    *      *      *      *
+//                    *      *      *      *
+//                    *      *      *      * 
+//////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+int main()
+{
+    for (int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                printf("* \t");
+            }
+            printf("\n");
+        }
+                    return 0;
+
+
+}
+
+Output =
+
+*       *       *       * 
+*       *       *       * 
+*       *       *       * 
+*       *       *       * 
