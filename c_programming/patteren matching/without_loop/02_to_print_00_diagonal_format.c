732e2b37d7a593ae4d9b1268ff65c279267b252e
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 17:21:18 +0530

    Update and rename 00_diagonal.c to 02_to_print_00_diagonal_format.c

---
+++ c_programming/patteren matching/without_loop/02_to_print_00_diagonal_format.c
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : To print 00 diagonal format
+// Concept : loops
+// Expected Output :   
+//                   00  
+//                        11  
+//                             22  
+//                                  33  
+//    
+//////////////////////////////////////////////////////////////
+
+
+
+
+
+#include<stdio.h>
+int main()
+{
+    printf("\t 00  \n");
+    printf("        \t 11  \n");
+    printf("                \t 22  \n");
+    printf("                        \t 33  \n");
+    return 0;
+    
+}
+
+
+Output :  00  
+                 11  
+                         22  
+                                 33  
