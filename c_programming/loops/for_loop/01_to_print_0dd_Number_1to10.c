996f53412fce3c69e05c7993afe9bd65a56bddba
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 16:18:27 +0530

    Update and rename 0dd_1to10.c to 01_to_print_0dd_Number_1to10.c

---
+++ c_programming/loops/for_loop/01_to_print_0dd_Number_1to10.c
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : For Loop
+// Concept : loops
+// Expected Output :  Print odd numbers between 1 and 10 using a for loop: 
+//                     1
+//                     3
+//                     5
+//                     7
+//                     9
+//////////////////////////////////////////////////////////////
+
+
+
+#include <stdio.h>
+
+int main() 
+{
+    printf("Print odd numbers between 1 and 10 using a for loop:\n");
+
+    for (int i = 1; i <= 10; i= i + 2) 
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
+
+
+Output = Print odd numbers between 1 and 10 using a for loop: 
+                     1
+                     3
+                     5
+                     7
+                     9
+       
