6d14c8d08cb4dcde7afa6a13c87730bc51f66a31
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 16:21:37 +0530

    Update and rename add_1to10.c to 03_to_print_addition_1to10.c

---
+++ c_programming/loops/for_loop/03_to_print_addition_1to10.c
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement :print 1 to 10 numbers addition
+// Concept : loops
+// Expected Output :  The Addition is : 55
+//////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+int main()
+{
+    int add = 0;
+    for(int i = 1;i <= 10 ;i++)
+    {
+         add += i;
+    }
+        printf(" The Addition is : %d \n",add);
+    return 0;
+}
+
+Output = 
+
+ The Addition is : 55
