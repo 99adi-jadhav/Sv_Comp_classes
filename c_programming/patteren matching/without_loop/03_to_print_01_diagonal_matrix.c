92cf6ddb52edfc1a840b3e459ada1256c12eb4ba
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 17:25:58 +0530

    Update and rename 01_diagonal.c to 03_to_print_01_diagonal_matrix.c

---
+++ c_programming/patteren matching/without_loop/03_to_print_01_diagonal_matrix.c
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : To print 01 diagonal matrix
+// Concept : loops
+// Expected Output :    
+//                        01  
+//                             02  
+//                                  03  
+//                                       04
+//    
+//////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+int main()
+{
+    printf("\t 00  \n");
+    printf("        \t 01  \n");
+    printf("                \t 02  \n");
+    printf("                        \t 03  \n");
+    return 0;
+    
+}
+
+output :          01  
+                         02  
+                                 03  
+                                        04
