9ed11ab3a80edbba90191451b9e7e735493f938c
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 17:34:16 +0530

    Update and rename daigonal.c to 06_to_print_a_daigonal_in_star_format.c

---
+++ c_programming/patteren matching/without_loop/06_to_print_a_daigonal_in_star_format.c
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : To print a daigonal in star format...!
+// Concept : loops
+// Expected Output :     *
+//                           *
+//                               *
+//                                   *
+//////////////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+int main()
+{
+    printf("\t *  \n");
+    printf("        \t *  \n");
+    printf("                \t *  \n");
+    printf("                        \t *  \n");
+    return 0;
+    
+}
+
+
+Output :
+               *  
+                   *  
+                         *  
+                               *  
