e28d7d6e6e24ef2a42863a5266501455908c32d4
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 17:19:18 +0530

    Update and rename star.c to 01_to_print_a_star_in_next_line.c

---
+++ c_programming/patteren matching/without_loop/01_to_print_a_star_in_next_line.c
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : to print a star in next line
+// Concept : loops
+// Expected Output :   
+//                *
+//                *    
+//                *
+//                *
+//                *
+//                *
+//////////////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+int main()
+{
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    return 0;
+    
+}
+
+Output : 
+
+*
+*
+*
+*
+*
+*
