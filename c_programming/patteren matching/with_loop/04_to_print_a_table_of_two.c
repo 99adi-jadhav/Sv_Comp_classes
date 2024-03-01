199756d5d6042b715c30208c8d6b18b28c0844b0
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 17:16:33 +0530

    Update and rename table.c to 04_to_print_a_table_of_two.c

---
+++ c_programming/patteren matching/with_loop/04_to_print_a_table_of_two.c
@@ -0,0 +1,39 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : to print  a table of two .....!
+// Concept : loops
+// Expected Output :   
+//    2
+//    4
+//    6
+//    8
+//    10
+//    12
+//    14
+//    16
+//    18
+//    20
+//////////////////////////////////////////////////////////////
+#include<stdio.h>
+    int main()
+        {
+           int i;
+              for(i = 2;i <= 20; i = i + 2)
+                {
+                    printf("%d \n",i);
+               }
+                     return(0);
+        }
+
+  Output :     
+    2
+    4
+    6
+    8
+    10
+    12
+    14
+    16
+    18
+    20
