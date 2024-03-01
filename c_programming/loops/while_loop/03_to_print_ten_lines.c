1253f22bf0fe00a90c43ee25df2447ca7df35ba3
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 17:06:24 +0530

    Update and rename ten_lines.c to 03_to_print_ten_lines.c

---
+++ c_programming/loops/while_loop/03_to_print_ten_lines.c
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : to print odd numbers between 1 and 10...!
+// Concept : loops
+// Expected Output :   Using a while loop:                    
+//                                            Line
+//                                            Line
+//                                            Line
+//                                            Line
+//                                            Line
+//                                            Line
+//                                            Line
+//                                            Line
+//                                            Line
+//                                            Line
+//////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+int main() 
+{
+    printf("\nUsing a while loop:\n");
+    int i;
+    while (i <= 10) {
+        printf("line \n", i);
+        i++;
+    }
+    return 0;
+}
+
+
+
+
+Output = 
+Using a while loop:
+ line
+ line
+ line
+ line
+ line
+ line
+ line
+ line
+ line
+ line
+ line
