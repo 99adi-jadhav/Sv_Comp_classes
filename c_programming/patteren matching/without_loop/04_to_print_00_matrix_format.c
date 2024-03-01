92c58d59bcd624a7a206f336fa44fc157e748a20
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 17:30:19 +0530

    Update and rename 01_format.c to 04_to_print_00_matrix_format.c

---
+++ c_programming/patteren matching/without_loop/04_to_print_00_matrix_format.c
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : To print 00 matrix format
+// Concept : loops
+// Expected Output :    
+//                           00      01      02      03 
+//                           10      11      12      13 
+//                           20      21      22      23 
+//                           30      31      33      34 
+//////////////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+int main()
+{
+    printf("\t 00 \t 01  \t 02 \t 03 \n");
+    printf("\t 10 \t 11  \t 12 \t 13 \n");
+    printf("\t 20 \t 21  \t 22 \t 23 \n");
+    printf("\t 30 \t 31  \t 33 \t 34 \n");
+    return 0;
+    
+}
+
+
+Output : 00      01      02      03 
+         10      11      12      13 
+         20      21      22      23 
+         30      31      33      34 
