b1c520e5ac130d4a2b72e9b02d235737f9aa13ba
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 16:25:05 +0530

    Update and rename five_lines.c to 04_to_print_five_lines.c

---
+++ c_programming/loops/for_loop/04_to_print_five_lines.c
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement :print 5 lines using for loop
+// Concept : loops
+// Expected Output :  Using a for loop :  line
+//                                        line
+//                                        line
+//                                        line
+//                                        line
+//////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main() 
+{
+    printf("Using a for loop:\n");
+    
+    for (int i = 1; i <= 5; i++) 
+    {
+        printf("Line %d\n", i);
+    }
+    return 0;
+}
+
+Output : Using a for loop :             line
+                                        line
+                                        line
+                                        line
+                                        line
