2bc605f713fcabedb8c5941dc61f0ce4b992ecc0
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 16:30:38 +0530

    Update and rename ten_lines.c to 06_to_print_ten_lines.c

---
+++ c_programming/loops/for_loop/06_to_print_ten_lines.c
@@ -0,0 +1,42 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement :print 10 lines using for loop
+// Concept : loops
+// Expected Output :  Using a for loop :  line
+//                                        line
+//                                        line
+//                                        line
+//                                        line
+//                                        line
+//                                        line
+//                                        line
+//                                        line
+//                                        line
+//////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main() 
+{
+    printf("Using a for loop:\n");
+
+    for (int i = 1; i <= 10; i++) 
+    {
+        printf("Line %d\n", i);
+    }
+    return 0;
+}
+
+
+Output = Using a for loop:
+Line                     
+Line
+Line
+Line
+Line
+Line
+Line
+Line
+Line
+Line
