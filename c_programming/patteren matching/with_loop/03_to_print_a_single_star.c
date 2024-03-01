5c5ca7f9cb3098de884d3cddab617069d8f3c35a
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 17:13:16 +0530

    Update and rename star.c to 03_to_print_a_single_star.c

---
+++ c_programming/patteren matching/with_loop/03_to_print_a_single_star.c
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : to print  a single star .....!
+// Concept : loops
+// Expected Output :  *      
+//////////////////////////////////////////////////////////////
+#include<stdio.h>
+int main()
+{
+    for(int i = 0; i <= 0; i++)
+    printf("* \n");   
+    return 0;
+    
+}
+
+Output :    *
