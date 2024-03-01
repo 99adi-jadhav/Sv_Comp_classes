4a1e20154a30cf43209d37d8ab56681fd4c062ae
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 16:20:15 +0530

    Update and rename 1to10_lines.c to 02_to_print_1to10_lines.c

---
+++ c_programming/loops/for_loop/02_to_print_1to10_lines.c
@@ -0,0 +1,43 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : For Loop
+// Concept : loops
+// Expected Output :  Print numbers 1 to 10 using a for loop: 
+//          1
+//          2
+//          3
+//          4
+//          5
+//          6
+//          7
+//          8
+//          9
+//          10
+//////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+int main() 
+{
+    printf("Print numbers 1 to 10 using a for loop:\n");
+
+    for (int i = 1; i <= 10; ++i) 
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
+
+Output = Print numbers 1 to 10 using a for loop: 1
+          2
+          3
+          4
+          5
+          6
+          7
+          8
+          9
+          10
