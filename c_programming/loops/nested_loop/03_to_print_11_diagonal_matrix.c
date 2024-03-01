a0e05056c88ad7b25e25062eaa057974abf1d3a4
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 16:39:47 +0530

    Update and rename 11_diagonal.c to 03_to_print_11_diagonal_matrix.c

--- c_programming/loops/nested_loop/03_to_print_11_diagonal_matrix.c
+++ c_programming/loops/nested_loop/03_to_print_11_diagonal_matrix.c
@@ -1,3 +1,17 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : to print 11 format diagonal Matrix....!
+// Concept : loops
+// Expected Output :    11 
+//                              22 
+//                                    33 
+//                                          44
+//////////////////////////////////////////////////////////////
+
+
+
+
 #include<stdio.h>
     int main()
  {
