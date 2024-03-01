05ce0bc51455255dda5e9948cc7183719c295ac2
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 16:41:18 +0530

    Update and rename diagonal_star.c to 04_to_print_diagonal_star.c

--- c_programming/loops/nested_loop/04_to_print_diagonal_star.c
+++ c_programming/loops/nested_loop/04_to_print_diagonal_star.c
@@ -1,3 +1,17 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : to print * form diagonal Matrix....!
+// Concept : loops
+// Expected Output :    * 
+//                             * 
+//                                    * 
+//                                          *
+//////////////////////////////////////////////////////////////
+
+
+
+
 #include<stdio.h>
     int main()
  {
