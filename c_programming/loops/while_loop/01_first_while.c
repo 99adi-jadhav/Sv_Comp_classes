40baf77064b8254044b1358f0a8ae193e0e5b494
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 16:55:36 +0530

    Update and rename first_while.c to 01_first_while.c

---
+++ c_programming/loops/while_loop/01_first_while.c
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : to print 0 to 9 Number...!
+// Concept : loops
+// Expected Output :   First while loop
+//		       0  1  2  3  4  5  6  7  8  9 
+//////////////////////////////////////////////////////////////
+
+
+
+#include<stdio.h>
+int main()
+{
+	printf("First while loop\n");
+
+	int i = 0;
+	while(i!=10)
+	{
+		printf(" %d ",i);
+		i++;
+	}
+}
+
+Output = 
+  First while loop
+ 0  1  2  3  4  5  6  7  8  9 
