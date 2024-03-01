7d0be4724a159e8ad6af612961c5374c64b56ec7
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-01 12:10:00 +0530

    Update 08_example_of_if_else_statement.c

--- c_programming/if_else_statements/08_example_of_if_else_statement.c
+++ c_programming/if_else_statements/08_example_of_if_else_statement.c
@@ -1,3 +1,11 @@
+//////////////////////////////////////////////////////////////
+// Date : 01 March 2024
+// Author : Jadhav Aditya Santosh
+// Problem Statement : Example Of if-else Statement....  
+// Concept : If-else statement Program
+// Expected Output :   The value is negative
+//////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 int main()
 {    
