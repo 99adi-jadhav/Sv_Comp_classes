3e37f80c2bc79af85a29c9d75db15718562b8c1c
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-24 18:47:53 +0530

    Create 02_int_arr.c

---
+++ c_programming/Array/02_int_arr.c
@@ -0,0 +1,30 @@
+/*Example : create an intger array
+    Date : 24 MARCH 2024                              
+    Author : Aditya s jadhav        
+*/      
+
+    #include<stdio.h>
+        int main()
+        {
+            int arr [6]; 
+                
+
+                arr [0] =10;
+                arr [1] =20;
+                arr [2] =30;
+                arr [3] =40;
+                arr [4] =50;
+                arr [5] =60;
+                arr [6] =70;
+
+
+            printf("\n Arr [0] = %d ",arr[0]);
+            printf("\n Arr [1] = %d ",arr[1]);
+            printf("\n Arr [2] = %d ",arr[2]);
+            printf("\n Arr [3] = %d ",arr[3]);
+            printf("\n Arr [4] = %d ",arr[4]);
+            printf("\n Arr [5] = %d ",arr[5]);
+            printf("\n Arr [6] = %d ",arr[6]);
+
+                return(0);
+        }
