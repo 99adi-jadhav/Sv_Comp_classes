f22712144ec58e321012780930f64d0e2ea78e4a
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-24 18:49:12 +0530

    Create 03_to_print_char.c

---
+++ c_programming/Array/03_to_print_char.c
@@ -0,0 +1,14 @@
+/*Example : create an Character array
+    Date : 24 MARCH 2024                              
+    Author : Aditya s jadhav        
+*/      
+
+#include<stdio.h>
+    int main()
+    {
+        char arr[] = "A";
+        
+            printf("%s",arr);
+
+                   return(0);
+    }
