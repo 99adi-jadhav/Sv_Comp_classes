57e5a7c8c8b77bbacbe544880d999825fbcaeb10
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-24 18:54:15 +0530

    Create 05_to_print_addition.c

---
+++ c_programming/Array/05_to_print_addition.c
@@ -0,0 +1,15 @@
+/*Example : Addition of two numbers
+    Date : 24 MARCH 2024                              
+    Author : Aditya s jadhav        
+*/    
+
+#include <stdio.h>
+
+int main() {
+    int arr[3] = {10, 20, 30 };
+    int sum = arr[0] + arr[1] + arr[2];
+    
+    printf("Sum of all numbers in the array: %d\n", sum);
+
+    return 0;
+}
