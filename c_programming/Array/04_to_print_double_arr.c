28ae79f92c5b3daae49a7ce50d7ded3fcf25e311
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-24 18:52:34 +0530

    Create 04_to_print_double_arr.c

---
+++ c_programming/Array/04_to_print_double_arr.c
@@ -0,0 +1,15 @@
+/*Example : create an Double array
+    Date : 24 MARCH 2024                              
+    Author : Aditya s jadhav        
+*/      
+
+#include <stdio.h>
+
+int main() {
+    
+    float arr[5] = {11.11, 22.22, 33.33};
+    
+    printf("%lf %lf %lf\n", arr[0], arr[1], arr[2]);
+    
+    return 0;
+}
