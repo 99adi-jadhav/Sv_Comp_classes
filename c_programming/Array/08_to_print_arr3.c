0ce201a266b6b7a744c3075fa4bfe45d0e9fbf8c
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-24 18:56:26 +0530

    Create 08_to_print_arr3.c

---
+++ c_programming/Array/08_to_print_arr3.c
@@ -0,0 +1,22 @@
+/*Example : create a simple program of array of line 3
+    Date : 24 MARCH 2024                              
+    Author : Aditya s jadhav        
+*/      
+
+    #include<stdio.h>
+        int main()
+        {
+            int arr [4]; 
+                
+
+                arr [0] =10;
+                arr [1] =20;
+                arr [2] =30;
+
+            printf("\n Arr [0] = %d ",arr[0]);
+            printf("\n Arr [1] = %d ",arr[1]);
+            printf("\n Arr [2] = %d ",arr[2]);
+            
+
+                return(0);
+        }
