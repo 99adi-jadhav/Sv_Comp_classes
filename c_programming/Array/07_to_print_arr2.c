df45517a32c0e80d005e0391485c75042d5f6811
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-24 18:55:47 +0530

    Create 07_to_print_arr2.c

---
+++ c_programming/Array/07_to_print_arr2.c
@@ -0,0 +1,20 @@
+/*Example : create a simple program of array of line 2
+    Date : 24 MARCH 2024                              
+    Author : Aditya s jadhav        
+*/      
+
+    #include<stdio.h>
+        int main()
+        {
+            int arr [2]; 
+
+                arr [0] =10;
+                arr [1] =20;
+            
+            printf("\n Arr [0] = %d ",arr[0]);
+            printf("\n Arr [1] = %d ",arr[1]);
+           
+            
+
+                return(0);
+        }
