be073d42d27d712d908a4eadfd030b6c26a2ef2a
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-24 18:55:06 +0530

    Create 06_to_print_float.c

---
+++ c_programming/Array/06_to_print_float.c
@@ -0,0 +1,15 @@
+/*Example : create an float array
+    Date : 24 MARCH 2024                              
+    Author : Aditya s jadhav        
+*/      
+
+#include <stdio.h>
+
+int main() {
+    
+    float arr[5] = {1.1, 2.2, 3.3};
+    
+    printf("%f %f %f\n", arr[0], arr[1], arr[2]);
+    
+    return 0;
+}
