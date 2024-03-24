97f388e56f264d4b61adbb2b3ffbbbf07b151513
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-24 18:40:27 +0530

    Create 01_initialisation_array.c

---
+++ c_programming/Array/01_initialisation_array.c
@@ -0,0 +1,16 @@
+/*Example : create a simple program of array 
+    Date : 24 MARCH 2024                              
+    Author : Aditya s jadhav        
+*/      
+
+   #include <stdio.h>
+
+int main()
+{
+    int arr[3] = {10,20,30};
+    
+    printf("array is : ");
+    printf("%d %d %d \n", arr[0], arr[1], arr[2]);
+    
+    return 0;
+}
