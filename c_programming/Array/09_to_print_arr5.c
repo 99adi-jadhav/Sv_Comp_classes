2a66ba8772d24fcfae8e869a69f872da5be721a1
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2024-03-24 18:57:00 +0530

    Create 09_to_print_arr5.c

---
+++ c_programming/Array/09_to_print_arr5.c
@@ -0,0 +1,27 @@
+/*Example : create a simple program of array of line 5
+    Date : 24 MARCH 2024                              
+    Author : Aditya s jadhav        
+*/      
+
+    #include<stdio.h>
+        int main()
+        {
+            int arr [5]; // [] = kiti block che member pahije tyasasthi
+                
+
+                arr [0] =10;
+                arr [1] =20;
+                arr [2] =30;
+                arr [3] =40;
+                arr [4] =50;
+
+
+            printf("\n Arr [0] = %d ",arr[0]);
+            printf("\n Arr [1] = %d ",arr[1]);
+            printf("\n Arr [2] = %d ",arr[2]);
+            printf("\n Arr [3] = %d ",arr[3]);
+            printf("\n Arr [4] = %d ",arr[4]);
+            
+
+                return(0);
+        }
